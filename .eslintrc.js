dfc1f229e629d231206ac158539605d01971e7c8
Ro Oeurn <ro@nowopen.studio>
2024-09-13 11:05:14 -0700

    newest

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = { 
- 
-  "rules": {
-     "@typescript-eslint/no-explicit-any": "off",
-       "@typescript-eslint/no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn"
-  
-      },
-}
