8e46d6fff52c3e61b4c0be2d48f3010d09a63dc7
Ro Oeurn <ro@nowopen.studio>
2024-09-23 12:26:22 -0700

    new

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -3,7 +3,7 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(profile:any) {
   try {
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
