11ad8438123fffb5cc44ae3d8c615fbb75af87ce
Ro Oeurn <ro@nowopen.studio>
2024-10-31 14:16:14 -0700

    new

--- app/structure/structure.tsx
+++ app/structure/structure.tsx
@@ -15,14 +15,14 @@ export default function Structure({sections}:any) {
 
     
   <div className="grid grid-cols-12 w-full items-center h-full">
-      {sections.map((item,i)=>{
+      {sections.map((item:any,i:any)=>{
         return(
         item.contentType === "text"?(
-          <div className="col-start-2 col-span-10  sm:px-0 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 xl:col-start-4 xl:col-span-6 mb-[40px] md:mb-[100px] md:mb-[60px]">
+          <div key={`text-${i}`}className="col-start-2 col-span-10  sm:px-0 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 xl:col-start-4 xl:col-span-6 mb-[40px] md:mb-[100px] md:mb-[60px]">
           <PortableText value={item.text}/>
           </div>
         ):(
-          <div className="col-span-12  mb-[40px]">
+          <div key={`img-${i}`} className="col-span-12  mb-[40px]">
           <Image alt="image" height={0}  width={0} sizes="100vw"  src={item.cover}  className="w-full object-fill"/>
           </div>
         )
