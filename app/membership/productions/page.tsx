4054b6e1670e3027bc2e498bc31d315df83de92c
Ro Oeurn <ro@nowopen.studio>
2025-01-31 11:29:24 -0800

    new

--- app/membership/productions/page.tsx
+++ app/membership/productions/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default async function Home() {
   const query = await getData(`{
     'settings':*[_type=="settings"]{contacts},
-    'preface':*[_type=="info" && slug.current=="preface"]{title,content,"imageURL":cover.asset->url},
+    'preface':*[_type=="info" && slug.current=="productions"][0]{title,content,"imageURL":cover.asset->url},
     'd_d':*[_type=="info" && slug.current=="d-d"]{title,content},
     'form':*[_type=="form"]{section,appSuccess, emailSub, emailer },
     'prod':*[_type=="project" && ( dept->slug.current == "productions")]{"logoUrl":logo.asset->url,title,desc, slug,"work":content[]{desc,"imageUrl":image.asset->url, video{asset->{playbackId}}}},
@@ -16,7 +16,7 @@ export default async function Home() {
     }`)
 
   const {preface, form, prod, settings, design, d_d} = query.data
-  console.log(preface[0].imageURL)
+
 
 
   return (
@@ -31,15 +31,20 @@ export default async function Home() {
             <div className="w-[100vw] grid grid-cols-12">
               <div className="px-[16px] sm:px-0 col-start-1 col-span-12 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 xl:col-start-4 xl:col-span-6 pt-[60px]">
               <div className={`text-nav text-center mb-[40px] border-[--black] border rounded-full uppercase w-full py-[8px] bottom-[--bar] px-[10px]`}>Productions</div>
-                          {preface[0].content.map((item:any, i:any)=>{
+                          {preface?(
+                            preface.content.map((item:any, i:any)=>{
                               return(
                                   <PortableText key={`item-${i}`} value={item.content}/>
                               )
-                          })}
+                          })
+                          ):('')}
   
                         
               </div>
-             <div className="col-span-full"> <Image alt="image" height={0}  width={0} sizes="100vw" src={preface[0].imageURL}  className="w-full object-fill"/></div>
+              {preface?(
+                <div className="col-span-full"> <Image alt="image" height={0}  width={0} sizes="100vw" src={preface.imageURL}  className="w-full object-fill"/></div>
+              ):('')}
+             
             </div>
           </div>
           // new
