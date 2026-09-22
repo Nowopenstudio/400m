17ca2648585dd90c82322468659aa5a643bdce55
yex884677 <61262949+Z26TH@users.noreply.github.com>
2026-09-22 06:58:50 -0400

    Update sanity.config.ts

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
 
   projectId: 'qzuc4ihq',
   dataset: 'production',
-  token:'skKTTyI8cXzzTi7fuSq4Q9znPhAVtnYrvYawK5Ye9iO695me8aDC9IowxOWZzhs8VMxNDtmgGwoCyW5NeDOfUHJtiFOO1eBjqBMElJ7YsioANtJxV0alziU3nOikTjUSWTR4F0VktdXOoPtJauAChNDIg84lk89vfJOVVJ0MXx5WPczMMJu1',
 
   plugins: [structureTool({
     structure: myStructure
